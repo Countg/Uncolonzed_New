7a0c4f15cf126fbe6ace8070fa1c3a3269de0801
Gavin Stephens <gstephens090@gmail.com>
2022-03-08 14:42:55 -0500

    axios update

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,32 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 import latestEpisodes from '../lib/latestEpisodes';
-import ContainerBlock from '../Components/ContainerBlock'
-import Hero from '../Components/Hero'
+import ContainerBlock from '../Components/ContainerBlock';
+import Hero from '../Components/Hero';
 import EpisodeContainer from '../Components/Episodes';
 import AboutSection from '../Components/AboutSection';
 import CallToAction from '../Components/CallToAction';
 
-
-export default function Home({episodeData}) {
+export default function Home({ episodeData }) {
   return (
-    <ContainerBlock 
-    title="Uncolonized Podcast - Race, pop culture and Politics, from a Canadian perspective"
-    description="Uncolonized is a funny and blunt podcast about race, politics, and culture, from a Canadian perspective."
-    >
-    <Hero episodes={episodeData}/>
-   
-    <AboutSection/>
-    <CallToAction/>
-    <EpisodeContainer episodes={episodeData}/>
-  
-   
+    <ContainerBlock
+      title='Uncolonized Podcast - Race, pop culture and Politics, from a Canadian perspective'
+      description='Uncolonized is a funny and blunt podcast about race, politics, and culture, from a Canadian perspective.'>
+      <Hero episodes={episodeData} />
+      <AboutSection />
+      <CallToAction />
+      <EpisodeContainer episodes={episodeData} />
     </ContainerBlock>
-  )
-};
+  );
+}
 
 export const getServerSideProps = async () => {
   const episodeData = await latestEpisodes();
-  
 
   return {
     props: {
       episodeData,
     },
-  }
-
-}
+  };
+};
