3dd23dcc75e63c460c1bb1559a38ab2846abd3ac
Gavin Stephens <gstephens090@gmail.com>
2022-05-04 13:10:24 -0400

    new commit

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import Hero from '../Components/Hero';
 import EpisodeContainer from '../Components/Episodes';
 import AboutSection from '../Components/AboutSection';
 import CallToAction from '../Components/CallToAction';
+import MessengerCustomerChat from 'react-messenger-customer-chat/lib/MessengerCustomerChat';
 
 export default function Home({ episodeData }) {
   return (
@@ -14,9 +15,14 @@ export default function Home({ episodeData }) {
       title='Uncolonized Podcast - Race, pop culture and Politics, from a Canadian perspective'
       description='Uncolonized is a funny and blunt podcast about race, politics, and culture, from a Canadian perspective.'>
       <Hero episodes={episodeData} />
+
       <AboutSection />
       <CallToAction />
       <EpisodeContainer episodes={episodeData} />
+      <MessengerCustomerChat
+        pageId='111002157049733"'
+        appId='714205756394335'
+      />
     </ContainerBlock>
   );
 }
